70e28e28df92bcaf47f17c0b053585f6b4438d2c
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-16 23:00:10 -0600

    Manejo de errores aritmeticos

---
+++ src/main/java/com/cursodespring/springboot/error/app/controllers/ErrorHandlerController.java
@@ -0,0 +1,21 @@
+package com.cursodespring.springboot.error.app.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+	
+	@ExceptionHandler(ArithmeticException.class)
+	public String arithmeticError(ArithmeticException exception, Model model){
+		model.addAttribute("error", "Error de Aritmética");
+		model.addAttribute("message", exception.getMessage());
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		model.addAttribute("timestamp", new Date());
+		return "error/aritmetica";
+	}
+}
